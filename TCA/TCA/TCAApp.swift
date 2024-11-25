ad1bac5d240ad40f77c2ae468e03a2ae5269daf4
Natalia Lazovskaya <natalia.lazovskaya@innowise.com>
2024-11-25 15:50:35 +0100

    add initial Xcode project

---
+++ TCA/TCA/TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAApp.swift
+//  TCA
+//
+//  Created by Nataliya Lazouskaya on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
